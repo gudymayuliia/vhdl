a16b50465b98935e9f7429668758e1e2e724bf69
Yuliia Hudyma <gudyma.yullia@gmail.com>
2025-01-07 15:46:17 +0200

    added clk input signal

--- spi_slave.vhdl
+++ spi_slave.vhdl
@@ -24,22 +24,25 @@ end spi_slave;
 
 architecture rtl of spi_slave is
 
-signal rx_data_s : std_logic_vector(N-1 downto 0);  -- received data
-signal bit_count : integer range 0 to N;
+signal rx_data_s : std_logic_vector(N-1 downto 0) := (others => '0');  -- received data
+signal bit_count : integer range 0 to N := 0;
 begin
 data_out  <= rx_data_s;
 SLAVE_INPUT : process(SCK)
 begin
 	if (reset = '1') then
 		rx_data_s <= (others => '0');
+		bit_count <= 0;
+		done_o <= '0';
 	elsif(SCK'event and SCK=CPOL) then 
 		rx_data_s <= rx_data_s(N-2 downto 0)&MOSI;
-		bit_count <= bit_count + 1;
-		if (bit_count = N-1) then
-			done_o <= '1';
-		else 
-			done_o <= '0';
-		end if;
+		if bit_count < N-1 then
+            bit_count <= bit_count + 1;
+            done_o <= '0'; -- Transmission not complete
+        else
+            bit_count <= 0; -- Reset counter after receiving full data
+            done_o <= '1'; -- Indicate completion
+        end if;
   end if;
 end process SLAVE_INPUT;
 
