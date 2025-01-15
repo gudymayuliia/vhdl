fe5bbb6e5e076a2528830a109bb1b4cc2524b735
Yuliia Hudyma <gudyma.yullia@gmail.com>
2025-01-15 09:48:58 +0200

    SPI slave module with clock domain crossing

--- spi_slave.vhdl
+++ spi_slave.vhdl
@@ -5,45 +5,90 @@
 --The SPI Slave should sample MOSI on the rising edge of SCK and assemble the data into 
 
 
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 entity spi_slave is
 generic(
   N : integer := 8;      
-  CPOL : std_logic := '1' );  
- port (
-   reset : in std_logic;
-   start : in std_logic;
-   data_out : out  std_logic_vector(N-1 downto 0); 
-   SCK : in  std_logic;
-   done_o : out std_logic;
-   MOSI : in  std_logic);
+  CPOL : std_logic := '0'  
+);
+port (
+  clk : in std_logic;  
+  reset : in std_logic; 
+  SCK : in std_logic; 
+  MOSI : in std_logic;
+  data_out : out std_logic_vector(N-1 downto 0);
+  done_o : out std_logic  
+);
 end spi_slave;
 
 architecture rtl of spi_slave is
 
-signal rx_data_s : std_logic_vector(N-1 downto 0) := (others => '0');  -- received data
+signal sck_shift_s : std_logic_vector(2 downto 0) := "000"; 
+signal sck_rising_edge : std_logic := '0';
+signal sck_falling_edge : std_logic := '0';
+signal rx_data_s : std_logic_vector(N-1 downto 0) := (others => '0');
 signal bit_count : integer range 0 to N := 0;
+signal done_s : std_logic := '0'; 
+
 begin
-data_out  <= rx_data_s;
-SLAVE_INPUT : process(SCK)
+data_out <= rx_data_s;
+done_o <= done_s;
+
+SCK_SHIFTING : process(clk)
 begin
-	if (reset = '1') then
-		rx_data_s <= (others => '0');
-		bit_count <= 0;
-		done_o <= '0';
-	elsif(SCK'event and SCK=CPOL) then 
-		rx_data_s <= rx_data_s(N-2 downto 0)&MOSI;
-		if bit_count < N-1 then
-            bit_count <= bit_count + 1;
-            done_o <= '0'; -- Transmission not complete
+    if rising_edge(clk) then
+        if reset = '1' then
+            sck_shift_s <= (others => '0');
         else
-            bit_count <= 0; -- Reset counter after receiving full data
-            done_o <= '1'; -- Indicate completion
+            sck_shift_s <= sck_shift_s(1 downto 0) & SCK;
         end if;
-  end if;
-end process SLAVE_INPUT;
+    end if;
+end process;
+
+SCK_EDGE_DETECT : process(clk)
+begin
+    if rising_edge(clk) then
+        if reset = '1' then
+            sck_rising_edge <= '0';
+            sck_falling_edge <= '0';
+        else
+            if sck_shift_s(2 downto 1) = "01" then
+                sck_rising_edge <= '1';
+					 sck_falling_edge <= '0';
+            elsif sck_shift_s(2 downto 1) = "10" then
+                sck_falling_edge <= '1';
+					 sck_rising_edge <= '0';	 
+            else
+                sck_falling_edge <= '0';
+					 sck_rising_edge <= '0';
+            end if;
+        end if;
+    end if;
+end process SCK_EDGE_DETECT;
+
+
+
+SLAVE_INPUT : process(clk)
+begin
+    if rising_edge(clk) then
+        if reset = '1' then
+            rx_data_s <= (others => '0');
+            bit_count <= 0;
+            done_s <= '0';
+        elsif sck_rising_edge = '1' then  
+            rx_data_s <= rx_data_s(N-2 downto 0) & MOSI;  
+            if bit_count < N-1 then
+                bit_count <= bit_count + 1;
+                done_s <= '0';
+            else
+                bit_count <= 0;
+                done_s <= '1';
+            end if;
+        end if;
+    end if;
+end process;
 
 end rtl;
